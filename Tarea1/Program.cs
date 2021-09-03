7ba52a18356d541473e4d0ff5a220b9b72df2159
RenzoPP <renzo-alfredo11@hotmail.com>
2021-09-03 05:14:56 -0500

    Agregar archivos de proyecto.

---
+++ Tarea1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Tarea1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int can;
+            int suma_par = 0;
+            string num;
+            int[] array = new int[10];
+            Console.WriteLine("Cuantos elementos: ");
+            can = int.Parse(Console.ReadLine());
+            //Ingresar numero al array
+            for(int i = 1; i < can; i++)
+            {
+                Console.WriteLine
+
+            }
+              
+        }
+
+        [Test]
+        public void Esnumeropar()
+        {
+            var utils = new StringUtilities();
+            var par = new int[] {1,2,3,4,5 };
+           var result = utils.SumaPar(par);
+           // Assert.AreNotEqual(6, result);
+            Assert.AreEqual(6, result);
+
+        }
+
+        [Test]
+        public void Esnumeropar2()
+        {
+            var utils = new StringUtilities();
+            var par = new int[] { 10, 20, 33, 7, 5 };
+            var result = utils.SumaPar(par);
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void Esnumeropar3()
+        {
+            var utils = new StringUtilities();
+            var par = new int[] { 10, 20, 33, 7, 5 };
+            var result = utils.SumaPar(par);
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void Esnumeropar4()
+        {
+            var utils = new StringUtilities();
+            var par = new int[] { 10, 20, 33, 7, 5 };
+            var result = utils.SumaPar(par);
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void Esnumeropar5()
+        {
+            var utils = new StringUtilities();
+            var par = new int[] { 10, 20, 33, 7, 5 };
+            var result = utils.SumaPar(par);
+            Assert.AreEqual(30, result);
+        }
+
+
+        
+
+    }
+}
