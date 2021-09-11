be7964b8e3d9ecdb84f07b2edb11937cfb16eaba
RenzoPP <renzo-alfredo11@hotmail.com>
2021-09-10 20:21:03 -0500

    Comienzo

---
+++ Examen_T1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
